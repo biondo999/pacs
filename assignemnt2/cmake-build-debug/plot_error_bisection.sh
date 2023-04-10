8cf0fb2c2d947fd325862e3017c8fc8c472a9438
Mariano <mariano.biondo@mail.polimi.it>
2023-04-10 21:31:37 +0000

    update

--- assignemnt2/cmake-build-debug/plot_error_bisection.sh
+++ assignemnt2/cmake-build-debug/plot_error_bisection.sh
@@ -4,6 +4,8 @@ set logscale y
 set key right top
 set grid
 set autoscale y
+set xlabel "iterations"
+set ylabel "log(error)"
 plot "error_bisection.dat" u 1:2  pt 8  lc 7 lw 2 with linespoints title "error(xk-x)"
 replot "error_bisection.dat" u 1:3 lw 3 lt 2 with linespoints title "upper bound by bisection"
 pause -1
