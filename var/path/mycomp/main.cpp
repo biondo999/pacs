4cf2ba814e63f1a33fef20be293d4566108ec6d7
Mariano <mariano.biondo@mail.polimi.it>
2023-04-20 22:37:27 +0000

    everythingup

--- var/path/mycomp/main.cpp
+++ var/path/mycomp/main.cpp
@@ -3,26 +3,43 @@
 
 double MySqr(double a_fVal) {  return a_fVal*a_fVal; }
 
+struct Function{
+mu::Parser p;
+double x=1;
+void init(const std::string &s){p.DefineVar("x", &x);p.SetExpr(s);}
+auto operator()(auto a){x=a;return p.Eval();}
+};
+
 int main()
 {
-    try
-    {
-        double var_a = 1;
+/*
+        double x = 1;
         mu::Parser p;
-        p.DefineVar("a", &var_a);
-        p.DefineFun("MySqr", MySqr);
-        p.SetExpr("MySqr(a)*_pi+min(10,a)");
+        std::string s="x*x";
+        p.DefineVar("x", &x);
+        //p.DefineFun("MySqr", MySqr);
+        p.SetExpr(s);
+       
 
-        for (std::size_t a=0; a<100; ++a)
-        {
-            var_a = a;  // Change value of variable a
-            std::cout << p.Eval() << std::endl;
+        for (std::size_t a=0; a<10; ++a)
+        {   x=a;
+            std::cout<<p.Eval() <<std::endl;
         }
-    }
-    catch (mu::Parser::exception_type &e)
-    {
-        std::cout << e.GetMsg() << std::endl;
-    }
+        */
+        std::string s="x*x";
+        Function f;  
+        f.init(s); 
+        s="ciao";
 
+        
+        for (std::size_t a=0; a<10; ++a){
+            std::cout<<f(a) <<std::endl;
+        }
+        
+        
+        
+        
+        
+        
     return 0;
 }
